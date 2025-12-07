786517c568f258e0d9bcf62155ed82faabd1c9aa
dirciano1 <51066028+dirciano1@users.noreply.github.com>
2025-12-07 16:45:11 -0300

    Delete utils/descontarCredito.ts

--- utils/descontarCredito.ts
+++
@@ -1,8 +0,0 @@
-import { db, doc, updateDoc, increment } from "@/lib/firebase";
-
-export async function descontarCredito(uid: string): Promise<void> {
-  const ref = doc(db, "users", uid);
-  await updateDoc(ref, {
-    creditos: increment(-1),
-  });
-}
